0dadbfc4c12aee28d25d876f3b19f4e03b5c7070
Franco Geier <francoyogeier@gmail.com>
2024-08-29 21:20:08 -0300

    Borrando cambios anteriores

--- js/script.js
+++ js/script.js
@@ -48,17 +48,3 @@ function ajustarAlturaTextArea() {
 }
 
 checkOrientacion();
-
-
-
-WebFontConfig = {
-    google: { families: ['Krub:400,700&display=swap'] }
-};
-(function() {
-    var wf = document.createElement('script');
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-})();
